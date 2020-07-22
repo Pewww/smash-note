220e54cb5715688bb7c383b88625f6960ce06d6b
Pewww <jung22337@naver.com>
2020-07-22 17:21:09 +0900

    :sparkles: Edo 폰트 추가 및 전역 설정

--- mini-rhythm-game/src/styles/global.styles.ts
+++ mini-rhythm-game/src/styles/global.styles.ts
@@ -1,11 +1,24 @@
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Edo';
+    src: url('../assets/fonts/Edo.ttf');
+  }
+
   html, body {
     width: 100%;
     height: 100%;
     margin: 0;
     padding: 0;
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  * {
+    font-family: 'Edo';
   }
 `;
 
